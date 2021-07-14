c2abb4fc2f885ed41384203f99f76571ad63cbaa
Tolik <tolikdolinskiy@gmail.com>
2021-07-14 21:44:21 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.159dcb8d507336399f3767cdf950b836.js"
 );
 
 self.addEventListener('message', (event) => {
